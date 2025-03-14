5ef1db2ed325c661e5265188fc8f7db0eba7f253
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-14 19:28:57 +0530

    fixed message endpoint

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { protectRoute } from "../middleware/auth.middleware";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSideBar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
