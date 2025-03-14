bf73c19ea8bc916d704937a36174ff4a225b8de4
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-14 19:03:31 +0530

    created endpoint to getMessages

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
 
@@ -12,4 +13,21 @@ export const getUsersForSideBar = async (req, res) => {
         res.status(500).json({ message: "internal server error" })
     }
 
+}
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const myId = req.user._id;
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, recieverId: userToChatId },
+                { senderId: userToChatId, recieverId: myId }
+            ]
+        })
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log("Error in getMessages controller ", error.message)
+        res.status(500).json({ message: "internal server error" })
+    }
 }
--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -4,5 +4,6 @@ import { protectRoute } from "../middleware/auth.middleware";
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSideBar)
+router.get("/:id", protectRoute, getMessages)
 
 export default router
