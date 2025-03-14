3f5ecfc4cb867cfe3efeb39c2474346e493a89ba
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-15 01:57:16 +0530

    done routing using react-router-dom

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import { HomePage } from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
+import SignUpPage from './pages/SignUpPage'
+import SettingPage from './pages/SettingPage'
 
 const App = () => {
   return (
-    <div className='text-red-500'>Hello</div>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/setting" element={<SettingPage />} />
+      </Routes>
+    </div>
   )
 }
 
