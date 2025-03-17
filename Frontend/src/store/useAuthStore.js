d77504cca885e906eb9dc3460efab37dc63b1806
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-18 02:07:41 +0530

    UI- built updateProfile page now we can update a profile image of a user

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -7,6 +7,7 @@ export const useAuthStore = create((set) => ({
     isSigningUp: false,
     isCheckingAuth: true,
     isloggingIn: false,
+    isUpdatingProfile: false,
 
     checkAuth: async () => {
         try {
@@ -56,5 +57,19 @@ export const useAuthStore = create((set) => ({
             set({ isloggingIn: false })
         }
 
+    },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true })
+        try {
+            const res = await axiosInstance.put('/auth/update-profile', data)
+            set({ authUser: res.data })
+            toast.success("Profile updated Successufly")
+        } catch (error) {
+            console.log("error in update profile", error)
+            toast.error(error?.response?.data?.message)
+        } finally {
+            set({ isUpdatingProfile: false })
+        }
     }
 }))
