c18d441091937db4721c8615a8efc9b26f79aded
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-20 19:56:19 +0530

    UI - created a store for messages and users

---
+++ Frontend/src/store/useChatStore.js
@@ -0,0 +1,36 @@
+import { create } from "zustand";
+import { axiosInstance } from '../libs/axios.js'
+import toast from 'react-hot-toast'
+
+
+export const useChatStore = create((set) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+
+    getUsers: async () => {
+        set({ isUsersLoading: true })
+        try {
+            const res = await axiosInstance.get('/messages/users');
+            set({ users: res.data })
+        } catch (error) {
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isUsersLoading: false })
+        }
+    },
+
+    getMessages: async (userId) => {
+        set({ isMessagesLoading: true })
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({ messages: res.data })
+        } catch (error) {
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isMessagesLoading: false })
+        }
+    }
+}))
