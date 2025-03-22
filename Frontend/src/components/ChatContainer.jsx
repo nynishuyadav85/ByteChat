b8ec0cadf05ebf50aed35db218c85523e387b12c
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-22 16:03:23 +0530

    UI- created chatContainer component

--- Frontend/src/components/ChatContainer.jsx
+++ Frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useChatStore } from '../store/useChatStore'
+import ChatHeader from './ChatHeader';
+import MessageInput from './MessageInput';
 
 const ChatContainer = () => {
+    const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore();
+    useEffect(() => {
+        getMessages(selectedUser._id)
+    }, [selectedUser._id, getMessages])
+
+    if (isMessagesLoading) {
+        return (
+            <div>
+                <ChatHeader />
+                Loading....
+                <MessageInput />
+            </div>
+        )
+    }
     return (
-        <div>ChatContainer</div>
+        <div>
+            <ChatHeader />
+            <p>messages....</p>
+            <MessageInput />
+        </div>
     )
 }
 
