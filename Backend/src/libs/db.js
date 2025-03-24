013f9bbb01e1071d7de399d47dff7e5fa58fe79d
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-24 21:09:31 +0530

    BE - added optional chaining

--- Backend/src/libs/db.js
+++ Backend/src/libs/db.js
@@ -4,9 +4,9 @@ export const connectBD = async () => {
 
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`DB connected ${conn.connection.host}`)
+        console.log(`DB connected ${conn?.connection.host}`)
     } catch (error) {
-        console.log(`DB error ${conn.connection.host}`)
+        console.log(`DB error ${conn?.connection.host}`)
     }
 
 }
