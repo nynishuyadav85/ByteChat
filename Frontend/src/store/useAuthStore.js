e6819348417a142e85ab622eb3885a94d5488502
Nishant Yadav <nynishuyadav85@gmail.com>
2025-03-26 08:52:43 +0530

    UI - fetching usersId to match it to socketID

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -84,7 +84,11 @@ export const useAuthStore = create((set, get) => ({
     connectSocket: () => {
         const { authUser } = get()
         if (!authUser || get().socket?.connected) return;
-        const socket = io(BASE_URL)
+        const socket = io(BASE_URL, {
+            query: {
+                userId: authUser._id
+            }
+        })
         socket.connect()
         set({ socket: socket })
     },
