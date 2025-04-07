5d1d87bfc9443728f6eba665a05fb6db170f8fd9
Nishant Yadav <nynishuyadav85@gmail.com>
2025-04-07 12:42:40 +0530

    fixed typo error

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from '../src/routes/auth.route.js'
 import messageRoutes from '../src/routes/message.route.js'
-import { connectBD } from './libs/db.js';
+import { connectDB } from './libs/db.js';
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { app, server } from './libs/socket.js';
@@ -25,5 +25,5 @@ app.use("/api/messages", messageRoutes)
 
 server.listen(PORT, () => {
     console.log("Server " + PORT)
-    connectBD();
+    connectDB();
 })
--- Backend/src/libs/db.js
+++ Backend/src/libs/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose"
 
-export const connectBD = async () => {
+export const connectDB = async () => {
 
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`DB connected ${conn?.connection.host}`)
     } catch (error) {
-        console.log(`DB error ${conn?.connection.host}`)
+        console.log(`DB connection error: ${error.message}`)
     }
 
 }
